4c579a37e75dc36ac1e9b7adb33183530acd26d7
Fatyma Ferdous <125547915+FatymaFerdous@users.noreply.github.com>
2023-08-02 14:22:17 +0500

    Commented Category

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config();
-const CategoryRouter = require('./api/category/Router')
+// const CategoryRouter = require('./api/category/Router')
 const UserRouter = require('./api/user/Router')
 const BrandRouter = require('./api/Brands/Router')
 const ProductRouter = require('./api/Products/Router')
@@ -10,7 +10,7 @@ const ProductRouter = require('./api/Products/Router')
 const port = process.env.SERVER_PORT || 3200
 
 app.use(express.json())
-app.use('/api', CategoryRouter)
+// app.use('/api', CategoryRouter)
 app.use('/api', UserRouter)
 app.use('/api', ProductRouter)
 app.use('/api', BrandRouter)
